594f9e932ce80f9b7edd0b69b649088565736914
Merlin Sparrow <anod100002@gmail.com>
2025-11-30 10:54:49 +0400

    chore: organize imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
